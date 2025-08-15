11a51feb2d8ee603973e497d8f8f9b102bdbb072
dishendra singh <dishendrasingh@gmail.com>
2025-08-15 19:06:58 +0530

    added new file

---
+++ 01_Basics/variable.js
@@ -0,0 +1,11 @@
+const my_Identity =123456
+let Name ="Dishendra singh"
+var age =35
+address = "Lasker"
+City ="Mumbai"
+
+console.log(my_Identity);
+
+console.table([my_Identity,Name,age,address,City])
+
+// Var type should not be used
