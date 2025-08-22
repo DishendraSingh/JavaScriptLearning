3e08baf87375458ea8beebb56f8b8d07405b7237
dishendra singh <dishendrasingh@gmail.com>
2025-08-22 23:58:40 +0530

    added new function

--- Functions/03_arrow.js
+++ Functions/03_arrow.js
@@ -12,3 +12,22 @@ user.welcome(); // Output: Welcome John Doe
 
 console.log(name); // Output: John Doe
 }
+//-----------------------------Arrow function-----------------------------
+
+const greet = () => {
+    console.log("Hello, World!");
+}
+greet(); // Output: Hello, World!
+
+const square = (x) =>
+
+    { 
+        return x * x
+
+    }
+    square(4); // Output: 16
+console.log(square(4)); // Output: 16
+const add = (a, b) => a + b;
+console.log(add(5, 3)); // Output: 8
+const returnObject = () => ({ name: "Alice", age: 25 });
+console.log(returnObject()); // Output: { name: 'Alice', age: 25 }
